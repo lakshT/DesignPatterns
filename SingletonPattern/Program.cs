18fa2200bdd01cfe63c8a577785c6459fe35a044
Lakshya Tandon <lakshya2691@live.com>
2020-02-23 22:13:56 -0700

    Adding singleton pattern to solution

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using SingletonPattern.Singletons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonExample.GetInstance().AddNumberToList(2);
+            SingletonExample.GetInstance().AddNumberToList(5);
+
+            foreach(int number in SingletonExample.GetInstance().GetList())
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
