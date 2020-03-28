a2706335f4dcd9f093aae41212af0cbc6cf1b00f
Lakshya Tandon <lakshya2691@live.com>
2020-03-28 17:44:53 -0600

    Adding template method pattern

---
+++ TemplateMethodPattern/Abstracts/CaffeineBeverage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethodPattern.Abstracts
+{
+    public abstract class CaffeineBeverage
+    {
+        public void Prepare()
+        {
+            Boil();
+            Brew();
+            Pour();
+            AddCondiments();
+        }
+
+        public void Boil()
+        {
+            Console.WriteLine("Water boiled.");
+        }
+
+        public abstract void Brew();
+
+        public void Pour()
+        {
+            Console.WriteLine("Poured in a Mug.");
+        }
+
+        public abstract void AddCondiments();
+    }
+}
