d596cb0f17061506c815654c97313c9cf4108357
Lakshya Tandon <lakshyatandon@nureva.com>
2020-04-02 18:25:50 -0600

    Adding Iterator pattern

---
+++ IteratorPattern/Iterators/PancakeHouseMenuIterator.cs
@@ -0,0 +1,33 @@
+﻿using IteratorPattern.Interfaces;
+using IteratorPattern.Menu;
+using System;
+using System.Collections.Generic;
+
+namespace IteratorPattern.Iterators
+{
+    public class PancakeHouseMenuIterator : IIterator
+    {
+        List<MenuItem> menuItems;
+
+        public PancakeHouseMenuIterator(List<MenuItem> menuItems)
+        {
+            this.menuItems = menuItems;
+        }
+
+        public bool HasNext()
+        {
+            if (menuItems.Count <= 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public object Next()
+        {
+            var menuItem = menuItems[0];
+            menuItems.Remove(menuItem);
+            return menuItem;
+        }
+    }
+}
---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using IteratorPattern.Interfaces;
+using IteratorPattern.Menu;
+using IteratorPattern.Menus;
+using System;
+
+namespace IteratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DinerMenu dinerMenu = new DinerMenu();
+            PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+
+            IIterator dinerMenuIterator = dinerMenu.CreateIterator();
+            IIterator pancakeHouseMenuIterator = pancakeHouseMenu.CreateIterator();
+
+            while (dinerMenuIterator.HasNext())
+            {
+                var menuItem = (MenuItem)dinerMenuIterator.Next();
+                Console.WriteLine("Name: " + menuItem.Name);
+                Console.WriteLine("Description: " + menuItem.Description);
+                Console.WriteLine("Price: " + menuItem.Price);
+                Console.WriteLine("Vegetarian: " + menuItem.IsVegetarian);
+                Console.WriteLine();
+            }
+
+            while (pancakeHouseMenuIterator.HasNext())
+            {
+                var menuItem = (MenuItem)pancakeHouseMenuIterator.Next();
+                Console.WriteLine("Name: " + menuItem.Name);
+                Console.WriteLine("Description: " + menuItem.Description);
+                Console.WriteLine("Price: " + menuItem.Price);
+                Console.WriteLine("Vegetarian: " + menuItem.IsVegetarian);
+                Console.WriteLine();
+            }
+        }
+    }
+}
